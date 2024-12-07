d0c2458cfe22669071d3f7fcd740036cc9d86f70
Pheonix-Tamer <jac.lewis.young@hotmail.co.uk>
2024-12-07 21:38:51 +0000

    Added CircleShape Class for future use

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}") 
+    
+    clock = pygame.time.Clock()
+    dt = 0 
     
     while(True):
         for event in pygame.event.get():
@@ -16,6 +19,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
+
 
 
 if __name__ == "__main__":
